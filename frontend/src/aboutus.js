0abda26e7ce8fe93c955ab5776be736668d9e118
Christopher Jasinski <89271300+Rytsumusic@users.noreply.github.com>
2023-09-15 00:33:29 -0400

    started some stuff made j general things

---
+++ frontend/src/aboutus.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function aboutus()
+{
+    return(
+        <div>
+        <Navbar></Navbar>
+      //  this is where the other content will go then in the app.js file we will call each of these files through Route path = "/" exact component= bracket frontpage bracket
+        
+        
+        </div>
+
+    );
+}
+
+
+export default aboutus;
