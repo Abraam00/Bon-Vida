a11f18918480434c073d05c4d2b7a5d9f224ea57
Rytsumusic <89271300+Rytsumusic@users.noreply.github.com>
2023-09-18 11:00:05 -0400

    just changed syntax

--- frontend/src/aboutus.js
+++ frontend/src/aboutus.js
@@ -2,10 +2,10 @@ import React from "react"
 
 function aboutus()
 {
+        //this is where the other content will go then in the app.js file we will call each of these files through Route path = "/" exact component= bracket frontpage bracket
+      //put it in between the divs
     return(
         <div>
-        <Navbar></Navbar>
-      //  this is where the other content will go then in the app.js file we will call each of these files through Route path = "/" exact component= bracket frontpage bracket
         
         
         </div>
--- frontend/src/frontpage.js
+++ frontend/src/frontpage.js
@@ -1,11 +1,10 @@
 import React from "react";
+      //  this is where the other content will go then in the app.js file we will call each of these files through Route path = "/" exact component= bracket frontpage bracket
 
 function frontpage()
 {
     return(
         <div>
-        <Navbar></Navbar>
-      //  this is where the other content will go then in the app.js file we will call each of these files through Route path = "/" exact component= bracket frontpage bracket
         
         
         </div>
